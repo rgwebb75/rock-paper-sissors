21585262617d5ff577eeb45f4c1a4df4fab882f2
rgwebb75 <91041344+rgwebb75@users.noreply.github.com>
2021-10-20 22:57:28 -0500

    Delete r-p-s.js

--- r-p-s.js
+++
@@ -1,7 +0,0 @@
-
-function computerPlay(){
-    //Return "rock", "paper" or "sissors" randomly
-    let arr = ["rock", "paper", "sissors"];
-    
-   return arr[Math.floor(Math.random() * arr.length)];
-}
